6fcf87ed2742cb36b7c5348bd3de3e7b8c2e71c9
VishnuVishwas <gvishnuvishwas60@gmail.com>
2024-05-28 21:31:01 +0530

    Push

---
+++ README.md
@@ -0,0 +1,14 @@
+# Keystroke Dynamics
+Keystroke-Based User Identification using Machine Learning.
+
+## Getting Started
+```sh
+
+# 1. Use local server to host the website, this records username and paragraph
+python ./run.py
+
+# 2. 
+
+# 4. test typing and predict who is the typer
+python ./test.py
+```
--- app/models/home.py
+++ app/models/home.py
@@ -7,10 +7,6 @@ import random
 import uuid
 import json
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
-import joblib
 
 random_paragraphs = [
     "c write know however late each that with because that place nation only for each change form consider we would interest with world so order or run more open that large write turn never over open each over change still old take hold need give by consider line only leave while what set up",
